7a6baa00ac5decc759fcc44bbac7e1b784f4dccc
RyKaT07 <patrykski07@gmail.com>
2024-11-18 13:33:53 +0100

    init

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="excel-lib",  
+    version="0.1.0",  
+    packages=find_packages(),
+    install_requires=[
+        "openpyxl>=3.0.0",
+        "pandas>=1.0.0",
+    ],
+    description="Library for working with Excel files using pandas and openpyxl",
+    author="Patryk Skibniewski",
+    author_email="patrykski07@gmail.com",
+    url="https://github.com/RyKaT07/google-api-lib",
+    python_requires=">=3.7"
+)
